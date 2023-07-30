7889734091c47ffcd189a497403af401c7d4fbdb
kirkl <kirklan0423@gmail.com>
2023-07-30 16:22:56 +0800

    BLE

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:ncue_aiot/screens/mqtt.dart';
 import 'package:ncue_aiot/screens/wifi_ssid_pw_via_ble.dart';
 // import 'package:ncue_aiot/screens/webview.dart';
 import '../services/local_auth_service.dart';
+import 'ble_device_detecter.dart';
 import 'bt_page.dart';
 import 'json_page.dart';
 import 'database.dart';
@@ -28,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         actions: const [
           PageButton(icon: Icons.account_tree_rounded, page: MqttPage()),
-          PageButton(icon: Icons.bluetooth, page: BTPage()),
+          PageButton(icon: Icons.bluetooth, page: BleDeviceList()),
           PageButton(icon: Icons.dataset, page: MysqlDemo()),
           // PageButton(icon: Icons.abc_sharp, page: WebViewTest())
         ],
